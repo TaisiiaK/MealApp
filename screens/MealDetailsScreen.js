0107d146545bd5f62228988d68dc0d742ec497ea
TaisiiaK <taisiiakompaniets@gmail.com>
2023-01-05 19:01:34 +0200

    created mealDetail file in MealDetail Screen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,10 +1,23 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, View, Text, StyleSheet } from 'react-native';
+
+import { MEALS } from '../data/dummy-data';
+import MealDetails from '../components/MealDetails';
 
 function MealDetailsScreen({ route }) {
   const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View>
-      <Text>This is Meal Deatail Screen {mealId}</Text>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredients</Text>
+      <Text>Steps</Text>
     </View>
   );
 }
