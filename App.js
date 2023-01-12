d201be4f802fcbf309c8bd22edfba0b8f34e7b14
TaisiiaK <taisiiakompaniets@gmail.com>
2023-01-12 17:46:15 +0200

    Managing favorite meals with context and install redux

--- App.js
+++ App.js
@@ -6,12 +6,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
+import { Provider } from 'react-redux';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealOverviewScreen from './screens/MealOverviewScreen';
 import MealDetailsScreen from './screens/MealDetailsScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
-import FavoritesContextProvider from './store/context/favorites-context';
+// import FavoritesContextProvider from './store/context/favorites-context';
+import store from './store/redux/store'
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -56,7 +58,8 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -80,7 +83,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+        </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   );
 }
--- screens/MealOverviewScreen.js
+++ screens/MealOverviewScreen.js
@@ -1,7 +1,6 @@
-import { View, StyleSheet, FlatList } from 'react-native';
 import { useLayoutEffect } from 'react';
 
-import MealItem from '../components/MealItem';
+import MealList from '../components/MealList/MealList';
 import { MEALS, CATEGORIES } from '../data/dummy-data';
 
 function MealOverviewScreen({ route, navigation }) {
@@ -14,41 +13,15 @@ function MealOverviewScreen({ route, navigation }) {
   useLayoutEffect(() => {
     const categoryTitle = CATEGORIES.find(
       (category) => category.id === categoId
-    )?.title;
+    ).title;
 
     navigation.setOptions({
       title: categoryTitle,
     });
   }, [categoId, navigation]);
 
-  function renderMealItem(itemData) {
-    const item = itemData.item;
-    const mealItemProps = {
-      id: item.id,
-      title: item.title,
-      imageUrl: item.imageUrl,
-      duration: item.duration,
-      complexity: item.complexity,
-      affordability: item.affordability,
-    };
-    return <MealItem {...mealItemProps} />;
-  }
-  console.log('displayedMeals', displayedMeals);
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealList items={displayedMeals}/>
 }
 export default MealOverviewScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
+
